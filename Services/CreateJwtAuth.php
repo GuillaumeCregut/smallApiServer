8e1bcaa05413b344593e2cb54160847011672d32
Guillaume Crégut <gcregut@free.fr>
2025-11-13 18:43:32 +0100

    create CreeatJWT class

---
+++ Services/CreateJwtAuth.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Services;
+use App\Services\GetEnvDatas;
+
+class CreateJwtAuth
+{
+    public function createToken(int $userId, array $role,?int $validity = 86400, ?string $firstname, ?string $lastname): string
+    {
+        $envs = new GetEnvDatas();
+        $secret = $envs->get('secret');
+        $payload = [
+            'user_id' => $userId,
+            'role' => $role,
+            'firstname' => $firstname,
+            'lastname' => $lastname
+        ];
+        $jwtToken = new JwtToken();
+        return $jwtToken->createToken($payload, $secret, $validity);
+    }
+}
