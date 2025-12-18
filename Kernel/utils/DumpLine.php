f8d1477fe6882ea7a490f20d560e428b79e5d7a3
Guillaume Crégut <gcregut@free.fr>
2025-12-18 22:10:44 +0100

    create dump classes

---
+++ Kernel/utils/DumpLine.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Kernel\utils;
+
+class DumpLine
+{
+    public ?string $line;
+    public mixed $value;
+    public ?string $type;
+    public ?string $name;
+
+    public function __construct(?string $line, mixed $value, ?string $type, ?string $name)
+    {
+        $this->name = $name;
+        $this->line = $line;
+        $this->value = $value;
+        $this->type = $type;
+    }
+}
