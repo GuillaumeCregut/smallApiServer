dc9971ad8df079acc25a3e8a761b297662c38bf7
Guillaume Crégut <gcregut@free.fr>
2026-01-07 23:41:12 +0100

    [Feat]: Auth middlewares: add session and http auth middleware

---
+++ middleware/SessionAuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Interfaces\ConnectorInterface;
+use App\Security\User;
+use App\Kernel\Interfaces\AuthenticationInterface;
+use App\Kernel\RequestObject;
+use App\Kernel\Traits\GetUserAuthTrait;
+
+class SessionAuthMiddleware implements AuthenticationInterface
+{
+    private ?User $user = null;
+
+    use GetUserAuthTrait;
+
+    public function __construct(private ConnectorInterface $connector)
+    {
+        // Initialization if needed
+    }
+    public function isAuth(): bool
+    {
+        $id = (int)RequestObject::getRequestInstance()->getSessionValue('user_id') ?? null;
+        $this->user = $this->getUserFromDB($id);
+       return $this->user !== null; 
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+}
