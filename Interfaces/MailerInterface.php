9e4876ee6bba403c5bb8767925a757e81bef4057
Guillaume Crégut <gcregut@free.fr>
2026-01-31 14:56:24 +0100

    [Fix]: MailerInterface: add file

---
+++ Interfaces/MailerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Interfaces;
+
+interface MailerInterface
+{
+    /**
+     * Envoie un email
+     *
+     * @param string $to Destinataire
+     * @param string $subject Sujet de l'email
+     * @param string $body Contenu de l'email
+     * @return bool Retourne true si l'email a été envoyé avec succès, false sinon
+     * @throws Exception Si une erreur survient lors de l'envoi de l'email
+     */
+    public function sendEmail(string $to, string $subject, string $body): bool;
+}
