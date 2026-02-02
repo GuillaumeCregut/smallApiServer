f11884e5e750116d5dc319f0029e2f2b1045cb27
Guillaume Crégut <gcregut@free.fr>
2026-02-02 22:41:16 +0100

    [Feat]: Kernel: create events and adapt to config loading

--- Kernel/Kernel.php
+++ Kernel/Kernel.php
@@ -3,12 +3,22 @@
 namespace App\Kernel;
 
 use App\Kernel\Request;
-use App\Services\Connector;
+use App\Kernel\Config\Events;
+use App\Kernel\Config\Router;
 use App\Kernel\GetClientParams;
 use App\Kernel\Responses\ErrorResponse;
+use App\Kernel\Config\DatabaseConnector;
 use App\Kernel\Interfaces\ResponseInterface;
+use App\Kernel\Psr14\Events\InitKernelEvent;
 use App\Kernel\Responses\ClientErrorResponse;
+use App\Kernel\Psr14\Listener\ListenerProvider;
+use App\Kernel\Psr14\Dispatcher\EventDispatcher;
+use App\Kernel\Psr14\Events\CallAuthKernelEvent;
+use App\Kernel\Psr14\Events\ConnectorKernelEvent;
+use App\Kernel\Psr14\Events\CheckApiKeyKernelEvent;
 use App\Kernel\Middleware\Security\AuthManagerMiddleware;
+use App\Kernel\Psr14\Events\ReturnResponseKernelEvent;
+use App\Kernel\Psr14\Events\StartControllerKernelEvent;
 
 class Kernel
 {
@@ -23,13 +33,17 @@ class Kernel
         $headers = GetClientParams::getheaders();
         $this->request = Request::initInstance($_SERVER, $datas, $_GET, $_POST, $_FILES, $_SESSION, $headers);
         $this->routeCall = $this->request->getURI();
+        $eventList = Events::getListeners();
+        MakeListener::applyListener($eventList);
     }
 
     public function route(): ResponseInterface
     {
-        // If required route is not is $routes, return a 404 Page not found error
+        $provider = ListenerProvider::getInstance();
+        EventDispatcher::getInstance($provider)->dispatch(new InitKernelEvent());
         if (!key_exists($this->routeCall, $this->routes)) {
             $response = new ClientErrorResponse(404);
+            EventDispatcher::getInstance()->dispatch(new ReturnResponseKernelEvent());
             return $response;
         }
         $matchingRoute = $this->routes[$this->routeCall];
@@ -38,16 +52,23 @@ class Kernel
 
         try {
             //Add auth middleware to request
-            $connector = new Connector();
+            EventDispatcher::getInstance()->dispatch(new ConnectorKernelEvent());
+            $connector = DatabaseConnector::getConnector();
+
+            EventDispatcher::getInstance()->dispatch(new CallAuthKernelEvent());
             $authMiddleware = AuthManagerMiddleware::getAuthMiddleware($connector);
             $this->request->setAuth($authMiddleware);
+            EventDispatcher::getInstance()->dispatch(new CheckApiKeyKernelEvent());
             // execute the controller
+            EventDispatcher::getInstance()->dispatch(new StartControllerKernelEvent());
             $page = (new $controller())->$method();
+            EventDispatcher::getInstance()->dispatch(new ReturnResponseKernelEvent());
             return $page;
         } catch (\Exception $e) {
             // if an exception is thrown during controller execution
-           $response = new ErrorResponse(500);
-           return $response;
+            $response = new ErrorResponse(500);
+            EventDispatcher::getInstance()->dispatch(new ReturnResponseKernelEvent());
+            return $response;
         }
     }
 }
