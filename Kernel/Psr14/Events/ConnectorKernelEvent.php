13c175d3ec960273d72e1b4d712f650337bc8a9d
Guillaume Crégut <gcregut@free.fr>
2026-02-02 22:33:05 +0100

    [Feat]: add kernel events

---
+++ Kernel/Psr14/Events/ConnectorKernelEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Kernel\Psr14\Events;
+
+class ConnectorKernelEvent extends AbstractStoppableEvent
+{
+     //Launched when kernel needs database connection 
+}
---
+++ Kernel/Psr14/Events/ReturnResponseKernelEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Kernel\Psr14\Events;
+
+class ReturnResponseKernelEvent extends AbstractStoppableEvent
+{
+     //Launched when kernel will return response to index
+}
