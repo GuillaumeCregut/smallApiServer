7857c6ae8e2c5e8188314b070f091b1b833f5127
Guillaume Crégut <gcregut@free.fr>
2026-02-02 15:39:47 +0100

    [Test]: ListenerProvider: add tests

--- tests/unit/kernel/Psr14/ListenerProviderTest.php
+++ tests/unit/kernel/Psr14/ListenerProviderTest.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('error_log', null);
-ini_set('log_errors', false);
 
 use PHPUnit\Framework\TestCase;
 use APP\Kernel\Psr14\Exceptions\EventException;
@@ -11,35 +8,33 @@ use App\Kernel\Interfaces\Psr14\StoppableEventInterface;
 
 class ListenerProviderTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        error_reporting(E_ALL);
-        ini_set('display_errors', '1');
-    }
+
     public function testProviderInstance(): void
     {
-        $provider= ListenerProvider::getInstance();
+        $provider = ListenerProvider::getInstance();
         $this->assertInstanceOf(ListenerProvider::class, $provider);
     }
 
     public function testProviderAddBadEvent(): void
     {
-       // $event = $this->createStub(StoppableEventInterface::class);
-       $event = $this->createStub(stdClass::class);
-       $listener = $this->createStub(ListenerInterface::class);
-        $provider=new ListenerProvider();
+        // $event = $this->createStub(StoppableEventInterface::class);
+        $event = $this->createStub(stdClass::class);
+        $listener = $this->createStub(ListenerInterface::class);
+        $provider = new ListenerProvider();
         $this->expectException(EventException::class);
-        $provider->addListener($event::class, $listener );
+        $provider->addListener($event::class, $listener);
     }
-     public function testProviderAddEvent()
+
+    public function testProviderAddEvent()
     {
         $event = $this->createStub(StoppableEventInterface::class);
         $listener = $this->createStub(ListenerInterface::class);
-        $provider=new ListenerProvider();
-        $provider->addListener($event::class, $listener );
+        $provider = new ListenerProvider();
+        $provider->addListener($event::class, $listener);
         $this->assertArrayHasKey($event::class, $provider->getListeners());
         $this->assertIsArray($provider->getListeners($event::class));
     }
+
     public function testProviderUnknwonClass(): void
     {
         $listener1 = $this->createStub(ListenerInterface::class);
@@ -52,11 +47,27 @@ class ListenerProviderTest extends TestCase
     {
         $event = $this->createStub(StoppableEventInterface::class);
         $listener = $this->createStub(ListenerInterface::class);
-        $provider=new ListenerProvider();
-        $provider->addListener($event::class, $listener );
+        $provider = new ListenerProvider();
+        $provider->addListener($event::class, $listener);
         // $this->assertArrayHasKey($event::class, $provider->getListeners());
         // $this->assertIsArray($provider->getListeners($event::class));
         $provider->clearListener($event::class);
         $this->assertArrayNotHasKey($event::class, $provider->getListeners());
+    }
+
+    public function testProviderAddEventInstance()
+    {
+        $event = $this->createStub(StoppableEventInterface::class);
+        $event2 = $this->createStub(StoppableEventInterface::class);
+        $listener = $this->createStub(ListenerInterface::class);
+        $provider = new ListenerProvider();
+        $provider->addListener($event::class, $listener);
+        $this->assertArrayHasKey($event::class, $provider->getListeners());
+        $this->assertIsArray($provider->getListeners());
+        $provider2 = ListenerProvider::getInstance();
+        $provider2->addListener($event2::class, $listener);
+        $provider3 = ListenerProvider::getInstance();
+        $this->assertArrayHasKey($event2::class, $provider3->getListeners());
+        $this->assertArrayHasKey($event::class, $provider3->getListeners());
     }
 }
