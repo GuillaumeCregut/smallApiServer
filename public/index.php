c92cbbd488bf878e0e8fd41f20af93a9e6532507
Guillaume Crégut <gcregut@free.fr>
2026-02-04 17:40:39 +0100

    [Style]: index: remove comment

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ session_set_cookie_params([
     'httponly' => true,
 ]);
 session_start();
-//avec la méthode orientée objet
+
 require_once '../vendor/Autoload.php';
 require_once '../Kernel/utils/Helpers.php';
 use App\Kernel\Kernel;
