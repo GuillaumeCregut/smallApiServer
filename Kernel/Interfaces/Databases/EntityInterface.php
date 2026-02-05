9af1a4b6714f1d43a0eeaecf6e53a444d4cc45d2
Guillaume Crégut <gcregut@free.fr>
2026-02-05 19:16:50 +0100

    [Feat]: Entity and repository interfaces

---
+++ Kernel/Interfaces/Databases/EntityInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Kernel\Interfaces\Databases;
+
+interface EntityInterface
+{
+    public function getId(): int;
+}
---
+++ Kernel/Interfaces/Databases/RepositoryInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Kernel\Interfaces\Databases;
+
+interface RepositoryInterface
+{
+ 
+    /**
+     * find entities by id
+     * @param int $id entity id
+     * @return EntityInterface  
+     */ 
+    public function find(int $id): EntityInterface;
+    
+     /**
+     * find entities by field
+     * @param array $fields : name of field to search value
+     * @return array<EntityInterface>
+     */ 
+    public function findBy(array $fields): array; 
+
+    /**
+     * Find all entities
+     *
+     * @return array<EntityInterface>
+     */ 
+    public function findAll(): array;
+
+     /**
+     * Save entity
+     * @param EntityInterface entity
+     * @return EntityInterface $new entity created
+     */ 
+    public function save(EntityInterface $entity): EntityInterface;
+
+     /**
+     * delete entity
+     * @param EntityInterface entity
+     *
+     */ 
+    public function delete(EntityInterface $entity): void;
+
+}
