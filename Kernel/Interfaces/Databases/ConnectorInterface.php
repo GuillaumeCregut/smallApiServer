d9c94157d49c2a1219daa71c125523f5c3168c7e
Guillaume Crégut <gcregut@free.fr>
2026-02-05 19:14:48 +0100

    [Feat]: Connector: add functions, not tested

--- Kernel/Interfaces/Databases/ConnectorInterface.php
+++ Kernel/Interfaces/Databases/ConnectorInterface.php
@@ -7,4 +7,10 @@ interface ConnectorInterface
 {
     public function getConnection(): mixed;
     public static function getInstance(): ConnectorInterface;
+    public function executeQuery(string $sql, array $params=[]): bool;
+    public function fetchQuery(string $sql, array $params=[]): array;
+    public function FetchQueryOnce(string $sql, array $params = []): ?array;
+    public function startTransac(): void;
+    public function commitTransc(): void;
+    public function rollBack(): void;
 }
