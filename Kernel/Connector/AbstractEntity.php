a0fad0e9bf223d6b3d6e48e864efe1984718bb4a
Guillaume Crégut <gcregut@free.fr>
2026-02-09 22:47:27 +0100

    [Feat]: AbtractEntity: Remove repo

--- Kernel/Connector/AbstractEntity.php
+++ Kernel/Connector/AbstractEntity.php
@@ -3,20 +3,14 @@
 namespace App\Kernel\Connector;
 
 use App\Kernel\Interfaces\Databases\EntityInterface;
-use App\Kernel\Interfaces\Databases\RepositoryInterface;
 
 abstract class AbstractEntity implements EntityInterface
 {
     protected ?int $id = null;
     protected string $repositoryClass;
-    protected RepositoryInterface $repository;
 
     public function __construct()
     {
-        if(!is_subclass_of($this->repositoryClass, RepositoryInterface::class)) {
-            throw new DatabaseException('Repository class must be an instance ofRepositoryInterface');
-        }
-        $this->repository=new $this->repositoryClass();
     }
 
     public function save(): self
