02100903b02d016b5918d16721a564e528ccdf32
Guillaume Crégut <gcregut@free.fr>
2026-02-10 17:52:25 +0100

    [Feat]: ErrorResponse: add exception

--- Kernel/Responses/ErrorResponse.php
+++ Kernel/Responses/ErrorResponse.php
@@ -4,17 +4,24 @@ namespace App\Kernel\Responses;
 
 use App\Kernel\utils\Dumper;
 use App\Kernel\AbstractResponse;
+use App\Kernel\GetEnvDatas;
 use App\Kernel\Interfaces\ResponseInterface;
+use Exception;
 
 class ErrorResponse extends AbstractResponse implements ResponseInterface
 {
     private array $statusMessages = [
         500 => ['HTTP/1.0 500 Internal Server Error', '500 - Internal Server Error'],
     ];
-    public function __construct(int $statusCode = 500)
+    public function __construct(int $statusCode = 500, ?Exception $e=null)
     {
         $this->setStatusCode(500);
-        $this->setBody($this->statusMessages[500][1]);
+        $body = $this->statusMessages[500][1];
+        $debug = GetEnvDatas::getEnvInstance()->get('debug_mode', false);
+        if ($debug && (null !== $e)) {
+            $body .= $this->makeDebugBody($e);
+        }
+        $this->setBody($body);
     }
 
     public function setBody(mixed $content): void
@@ -30,5 +37,10 @@ class ErrorResponse extends AbstractResponse implements ResponseInterface
     {
         
         Dumper::displayHTML();
+    }
+
+    private function makeDebugBody(Exception $e): string
+    {
+        return '';
     }
 }
