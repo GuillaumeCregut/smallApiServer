8903391cc5376eff02246f524ba7d420ce6cc732
Guillaume Crégut <gcregut@free.fr>
2026-02-12 22:57:02 +0100

    [Feat]: UserRepo: add findbyUsernameCredentials

--- Security/UserRepository.php
+++ Security/UserRepository.php
@@ -26,6 +26,37 @@ class UserRepository extends AbstractRepository
         if(1<count($result)) {
             throw new DatabaseException('More than one user found');
         }
-        return $result[0];
+        $user = $result[0];
+        /**
+         * @var User $user
+         */
+        $user->setPassword(null);
+        return $user;
+    }
+
+    public function findByUserNameCredentials(string $username, string $password): ?User
+    {
+         $search = [
+            'username' => $username
+        ];
+        $result = $this->findBy($search);
+        if(empty($result)) {
+            return null;
+        }
+        if(1<count($result)) {
+            throw new DatabaseException('More than one user found');
+        }
+       /**
+        * @var User $user
+        */ 
+       $user = $result[0];
+       //Check password
+        $dbPass = $user->getPassword();
+        if(!password_verify($password, $dbPass)) {
+            return null;
+        }
+       //return new User
+       $user->setPassword(null);
+       return $user;
     }
 }
