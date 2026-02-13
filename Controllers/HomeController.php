96e213499ca4aca500f63eb528bdd32be9719a32
Guillaume Crégut <gcregut@free.fr>
2026-02-13 22:53:03 +0100

    [Feat]: HomeController: adapt to make example

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controllers;
 
-use App\Models\HomeModel;
 use App\Kernel\Responses\JsonResponse;
-use App\Kernel\Responses\ErrorResponse;
 use App\Kernel\AbstractController;
-use App\Kernel\Config\DatabaseConnector;
 use App\Kernel\Interfaces\ResponseInterface;
 use App\Kernel\Responses\ClientErrorResponse;
 
@@ -16,107 +13,57 @@ class HomeController extends AbstractController
     public function __construct()
     {
         parent::__construct();
-        $this->connector = DatabaseConnector::getConnector();
-    }
-    public function index(): ResponseInterface
-    {
-        //Ici, on vérifie la méthode utilisée 
-        switch ($this->request->getMethod()) {
-            case 'GET':
-                return $this->getDatas();
-            case 'POST':
-                return $this->addData();
-            case 'PUT':
-                return $this->changeData();
-            case 'PATCH':
-                return $this->changeData();
-            case 'DELETE':
-                return $this->deleteData();
-            default:
-                return $this->returnError(405);
-        }
     }
 
-    private function getDatas(): ResponseInterface
+    public function getDatas(): ResponseInterface
     {
-        //example of authentication check
-        // if (!$this->isUserAuth()) {
-        //     return $this->returnError(401);
-        // }
-        $model = new HomeModel($this->connector->getConnection());
-        //Ici, c'est presque le plus compliqué, car si on demande une seule donnée, il ne faut pas retourner un tableau, mais juste l'entité
-        $datas = []; //Le container de données de la BDD
-        if (key_exists('id', $this->request->getAllDatas())) {
-            //on récupère une donnée en particulier
-            $id = (int) $this->request->getAllDatas()['id'];
-            
-            $datas = $model->getOne($id); // On récupère la donnée en BDD
-            if (empty($datas)) {
-                //si la donnée n'existe pas
-                $response = new ClientErrorResponse(404);
-                return $response;
-            }
+        //check if we want only one data or all datas
+        if($this->request->getData('id')) {
+            //get one data from DB
+            $datas = '';
         } else {
-            //on récupère toutes les données
-            $datas = $model->getAll();
+           //Get all datas from DB
+            $datas = [];
         }
-        $response = new JsonResponse();
-        $response->setBody($datas);
-        return $response;
+        return $this->returnJson($datas,200);
     }
 
-    private function addData(): ResponseInterface
+    public function addData(): ResponseInterface
     {
-        //On verifie que toutes les données nécessaires sont présentes et on ajoute
+        //Check incoming datas
         $error = false;
         $datas = $this->request->getAllDatas();
-        //Si il manque des données 
+        //In case wrong datas 
         if ($error) {
             return new ClientErrorResponse(404);
         } else {
-            //On ajoute la donnée
-            $model = new HomeModel($this->connector->getConnection());
-            $model->add($datas);
-            if (!$model) {
-               return new ErrorResponse();
-            }
-            return new JsonResponse(201);
+            //Add entity to DB
+            $newData= [];
+            return $this->returnJson($newData, 201);
         }
     }
 
-    private function changeData(): ResponseInterface
+    public function changeData(): ResponseInterface
     {
-        //On verifie que toutes les données nécessaires sont présentes et on modifie
+        //Check incoming datas
         $error = false;
         $datas = $this->request->getAllDatas();
-        //Si il manque des données 
+        //In case wrong datas 
         if ($error) {
             return new ClientErrorResponse(422);
         } else {
-            //On modifie la donnée
-            $model = new HomeModel($this->connector->getConnection());
-            $model->update((int)$datas['id'], $datas);
-            if (!$model) {
-                return new ErrorResponse();
-            }
-            $response = new JsonResponse();
-            return $response;
+            //Update Database;
+            return $this->returnJson(204);
         }
     }
-    private function deleteData(): ResponseInterface
+    public function deleteData(): ResponseInterface
     {
-        if (key_exists('id', $this->request->getAllDatas())) {
-            //on supprime une donnée en particulier
-            $id = (int) $this->request->getAllDatas()['id'];
-            $model = new HomeModel($this->connector->getConnection());
-            $model->delete($id);
-            if (!$model) {
-               return new ErrorResponse();
-            }
-        } else {
-            return new ClientErrorResponse(404);
-        }
-        $response = new JsonResponse();
+        //example of authentication check
+        if (!$this->isUserAuth()) {
+            return $this->returnError(401);
+        } 
+        //Do something that delete entity from database
+        $response = new JsonResponse(204);
         return $response;
     }
 }
