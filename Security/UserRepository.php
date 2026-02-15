52caa74ce5de9888f39eddfb1dba38e0cce619df
Guillaume Crégut <gcregut@free.fr>
2026-02-15 23:01:17 +0100

    [Feat]: UserRepository: add findall, save

--- Security/UserRepository.php
+++ Security/UserRepository.php
@@ -25,22 +25,22 @@ class UserRepository extends AbstractRepository
         return $user;
     }
 
-     public function findBy(array $fields): array
-     {
+    public function findBy(array $fields): array
+    {
         $users = parent::findBy($fields);
         return $users;
-     }
-   
+    }
+
     public function findOneByEmail(string $email): ?EntityInterface
     {
         $search = [
             'email' => $email
         ];
         $result = $this->findBy($search);
-        if(empty($result)) {
+        if (empty($result)) {
             return null;
         }
-        if(1<count($result)) {
+        if (1 < count($result)) {
             throw new DatabaseException('More than one user found');
         }
         $user = $result[0];
@@ -52,26 +52,55 @@ class UserRepository extends AbstractRepository
 
     public function findByUserNameCredentials(string $username, string $password): ?User
     {
-         $search = [
+        $search = [
             'username' => $username
         ];
         $result = $this->findBy($search);
-        if(empty($result)) {
+        if (empty($result)) {
             return null;
         }
-        if(1<count($result)) {
+        if (1 < count($result)) {
             throw new DatabaseException('More than one user found');
         }
-       /**
-        * @var User $user
-        */ 
-       $user = $result[0];
-       //Check password
+        /**
+         * @var User $user
+         */
+        $user = $result[0];
+        //Check password
         $dbPass = $user->getPassword();
-        if(!password_verify($password, $dbPass)) {
+        if (!password_verify($password, $dbPass)) {
             return null;
         }
-       //return new User
-       return $user;
+        //return new User
+        return $user;
+    }
+
+    public  function findAll(): array
+    {
+        $users = parent::findAll();
+        return $users;
+    }
+
+    public function save(EntityInterface $entity): null | false | EntityInterface
+    {
+        if(!$entity instanceof User) {
+            throw new \InvalidArgumentException('Entity must be UserEntity');
+        }
+        if(null !== $entity->getNewPassword()) {
+            $hashedPassword = password_hash($entity->getNewPassword(),PASSWORD_DEFAULT);
+            $entity->setPassword($hashedPassword);
+            $entity->setNewPassword(null);
+        } else if (null !== $entity->getId()) {
+            /**
+             * @var User $existingUser
+             */
+            $existingUser = $this->find($entity->getId());
+            if( $existingUser) {
+                $pass = $existingUser->getPassword();
+                $entity->setPassword($pass);
+            }
+        }
+        $newEntity = parent::save($entity);
+        return $newEntity;
     }
 }
