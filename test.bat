0a7485142f10e20d4801796e3a087f282db026b8
Guillaume Crégut <gcregut@free.fr>
2026-02-15 19:54:41 +0100

    [Test]: Display warnings

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 @echo off
 if "%1"=="" (echo "test tous les fichiers") else (echo "test fichier %1")
-php ./tools/phpUnit.phar %1 -c ./tools/phpUnit.xml --bootstrap ./tests/bootstrap.php
+php ./tools/phpUnit.phar %1 -c ./tools/phpUnit.xml --display-warnings --bootstrap ./tests/bootstrap.php
