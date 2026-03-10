c6de7fe7133d14d028c4fed2de511409334c2e69
Guillaume Crégut <gcregut@free.fr>
2026-03-10 18:06:09 +0100

    [Feat]: Equals: create and tests

---
+++ src/Kernel/Form/Validator/Assert/Equals.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @license MIT
+ * Copyright (c) 2026 Guillaume Crégut
+ */
+
+namespace App\Kernel\Form\Validator\Assert;
+
+use \Attribute;
+
+#[Attribute]
+class Equals extends AbstractAssert
+{
+    private mixed $expected = null;
+    public function __construct(mixed $expected,?string $errorMessage = null)
+    {
+        $this->expected = $expected;
+        $this->errorMessage = $errorMessage ?? "property %s must be equals to " . (is_scalar($expected) || $expected === null ? (string)$expected : gettype($expected));
+        $this->allowedTypes = ['string', 'double', 'integer', 'boolean', 'NULL'];
+    }
+
+    public function validate(mixed $value): bool 
+    {
+        if (!$this->isValidType($value)) return false;
+        return $this->check($value);
+    }
+
+    public function check(mixed $value): bool
+    {
+        return $value === $this->expected;
+    }
+}
